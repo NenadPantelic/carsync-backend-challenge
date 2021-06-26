6b47026e250bfe2bf47048ee07f3bca575c1033f
NenadPantelic <nenadpantelickg@gmail.com>
2021-06-26 12:24:00 +0200

    CS#006: Add db seed utils with initial user dump.

--- src/main/java/com/carsync/challenge/api/ApiApplication.java
+++ src/main/java/com/carsync/challenge/api/ApiApplication.java
@@ -1,13 +1,31 @@
 package com.carsync.challenge.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.carsync.challenge.api.utils.DbSeedUtils;
 
 @SpringBootApplication
 public class ApiApplication {
+
+	@Autowired
+	private DbSeedUtils _dbSeedUtils;
+
+	private DbSeedUtils getDbSeedUtils() {
+		return _dbSeedUtils;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			getDbSeedUtils().addUser();
+		};
+	}
 }
