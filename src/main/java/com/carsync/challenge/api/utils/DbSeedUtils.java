d8de061e2c7612e95e7634aefe858f098ffb074f
NenadPantelic <nenadpantelickg@gmail.com>
2021-06-27 18:35:09 +0200

    CS#006: Add two FA token initial dump.

--- src/main/java/com/carsync/challenge/api/utils/DbSeedUtils.java
+++ src/main/java/com/carsync/challenge/api/utils/DbSeedUtils.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import com.carsync.challenge.api.dao.TwoFARepository;
 import com.carsync.challenge.api.dao.UserRepository;
+import com.carsync.challenge.api.model.TwoFAToken;
 import com.carsync.challenge.api.model.User;
+import com.carsync.challenge.api.model.utils.TokenType;
 
 import lombok.Getter;
 import lombok.experimental.Accessors;
@@ -18,11 +21,18 @@ public class DbSeedUtils {
 	private UserRepository _userRepository;
 
 	@Autowired
+	private TwoFARepository _twoFARepository;
+
+	@Autowired
 	private PasswordEncoder _passwordEncoder;
 
 	public void addUser() {
 		getUserRepository().save(
 				User.builder().email("test@test.com").password(getPasswordEncoder().encode("password123")).build());
+	}
+
+	public void addTwoFAToken() {
+		getTwoFARepository().save((TwoFAToken) FactoryUtils.createToken(TokenType.TWO_FA, "+38167999999", 180));
 	}
 
 }
