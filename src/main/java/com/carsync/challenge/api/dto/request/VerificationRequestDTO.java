0acf31f35107db43a89fcce4e77c804e7ffc358e
NenadPantelic <nenadpantelickg@gmail.com>
2021-06-27 19:15:49 +0200

    CS#012: Add token to VerificationRequestDTO.

--- src/main/java/com/carsync/challenge/api/dto/request/VerificationRequestDTO.java
+++ src/main/java/com/carsync/challenge/api/dto/request/VerificationRequestDTO.java
@@ -1,5 +1,18 @@
 package com.carsync.challenge.api.dto.request;
 
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Accessors(prefix = "_")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VerificationRequestDTO {
 
+	@NotBlank(message = "Verification token is required.")
+	private String _verificationToken;
 }
